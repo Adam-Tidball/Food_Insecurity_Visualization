0af4491969151ef4bbbfa47957ae1f4b46865af4
George Wiseman <georgeawiseman@gmail.com>
2024-07-25 13:58:01 -0700

    fixes

--- static/src/js/circle_chart.js
+++ static/src/js/circle_chart.js
@@ -29,82 +29,76 @@ let xScale = d3.scaleLinear().domain([125, 155]).range([margin.left, width - mar
 let yScale = d3.scaleLinear().domain([5, 30]).range([height - margin.bottom, margin.top])
 
 
-let circle_chart = d3.select("#health_circle_chart")
+let svg = d3.select("#health_circle_chart")
     .classed("border", true)
     .attr("height", height + margin.top + margin.bottom)
     .attr("width", width + margin.left + margin.right)
 ;
 
-const xAxis = d3.select("#health_circle_chart")
+const xAxis = svg
     .append("g")
     .attr("transform", `translate(0,${height - margin.bottom})`)
     .call(d3.axisBottom(xScale));
 
-const yAxis = circle_chart.append('g')
+const yAxis = svg
+    .append('g')
     .attr("transform", `translate(${margin.left},0)`)
     .call(d3.axisLeft(yScale))
 ;
 
+const t = svg.transition().duration(750);
 
-let circle_gs = d3.select("#health_circle_chart")
-    .selectAll("#circle_marks")
-    .data(dataAt(year))
-    .join("g");
-
-circle_gs.append('circle')
-        .sort((a,b) => {d3.descending(a.Geography, b.Geography)})     
-        .classed("circle_marks", true)
+const circle_pos_size = function (circle_sel){
+    console.log(circle_sel)
+    circle_sel
         .attr("id", "circle_marks")
         .attr("year_prov", d=> d.Year + "_" + d.Geography)
         .attr("r", d => rScale(Math.pow(d.DiabetesPercent, 0.6)))
         .attr("cx", d => xScale(d.Cpi))
         .attr("cy", d => (yScale(d.FoodInsecure)))
+        .attr("class", "circle_marks");
+}
 
-        .on("mouseover", function(d){
-            d3.select(this)
-                .attr("opacity", 0.75)
-            d3.select(this.parentNode).selectAll("text")
-                .attr("opacity", 1)
-        })
-        .on("mouseout", function(d){
-            d3.select(this.parentNode).selectAll("text")
-                .attr("opacity", 0)
-
-        });
-
-circle_gs.append('text')
-    .text(d => d.Geography)
-    .attr("x", d => xScale(d.Cpi))
-    .style("pointer-events", "none")
-    .attr("y", d =>yScale(d.FoodInsecure))
-    .attr("opacity", 0);
-
-console.log(Math.pow(11.5, 0.6))
+const text_pos_size = function(text_sel){
+    text_sel.text(d => d.Geography)
+        .attr("x", d => xScale(d.Cpi))
+        .style("pointer-events", "none")
+        .attr("y", d =>yScale(d.FoodInsecure))
+        .attr("opacity", 0);
+}
 
 const updateCircles = function updateCircles(year){
-    console.log(year)
-    circle_gs.data(dataAt(year)).join("g")
-    
-    let update_c = function update_c(selection) {
-        selection.select("circle")
-            .attr("r", d => rScale(Math.pow(d.DiabetesPercent, 0.6)))
-            .attr("year_prov", d=> d.Year + "_" + d.Geography)
-            .attr("cx", d => xScale(d.Cpi))
-            .attr("cy", d => yScale(d.FoodInsecure))
-    }
-
-    let update_t = function update_t(selection) {
-        selection.select("text")
-            .attr("x", d => xScale(d.Cpi))
-            .attr("y", d => (yScale(d.FoodInsecure)))
-    }
-    circle_gs.exit()
-        .transition()
-
-    circle_gs.transition()
-        .duration(1000)
-        .call(update_c)
-        .call(update_t)
+    let circle_gs = d3.select("#health_circle_chart")
+    .selectAll("#circle_gs")
+    .data(dataAt(year)) 
+    .join(
+        enter => {
+            var g = enter.append("g")
+                .attr("id", "circle_gs"); 
+            const circles = g.append('circle')
+                .on("mouseover", function(d){
+                    d3.select(this)
+                        .attr("opacity", 0.75)
+                    d3.select(this.parentNode).selectAll("text")
+                        .attr("opacity", 1)
+                })
+                .on("mouseout", function(d){
+                    d3.select(this.parentNode).selectAll("text")
+                        .attr("opacity", 0)
+                });
+            circle_pos_size(circles)
+            const text = g.append('text');
+            text_pos_size(text);
+        },
+        update => {
+            update.transition()
+                .call( update => update.select("#circle_marks").call(circle_pos_size))
+                .call( update => update.select("text").call(text_pos_size))
+        },
+        exit => {
+            exit.remove("*")
+        }
+    );
 
     document.getElementById("listenbutton")
         .innerText = year
@@ -117,15 +111,11 @@ document.getElementById("slide_mouse")
     .addEventListener("mousemove", onMousemove, false);
 
 
-year = 2018
+updateCircles(2018);
 function onMousemove(event){
     let rect = event.target.getBoundingClientRect();
     let x = Number(event.offsetX);
-    
-    console.log("X Position: " + typeof(x));
-    console.log(typeof(rect.width));
-     
-    console.log( x / rect.width);
+
 
     const year_to_use = Math.round( (x / rect.width) * (2022 - 2018) + 2018)
     if (year_to_use != year){
